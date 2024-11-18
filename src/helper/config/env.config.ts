2ba8a3b4b49032733d84401189a738656aee2a7f
Rakibul Islam <fl.rakibul@gmail.com>
2024-11-18 13:44:34 +0600

    APIs url updated

--- src/helper/config/env.config.ts
+++ src/helper/config/env.config.ts
@@ -1,5 +1,6 @@
 export const getBaseUrl = () => {
   return (
-    process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:7000/api/v1/"
+    process.env.NEXT_PUBLIC_API_BASE_URL ||
+    "https://user-manage-m53i.onrender.com/api/v1/"
   );
 };
